15a7b4e7fe372903ab41caac92d58952c90cc3c5
Illia Ostapyshyn <illia@yshyn.com>
2024-11-22 13:34:44 +0100

    common: Add flake.lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "SRA packages";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
   outputs = { self, nixpkgs, ... }: let
     supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
