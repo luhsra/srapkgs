bad5ec076b2024ac97cb77d5734ebe6bd8651041
Kenny Albes <albes@sra.uni-hannover.de>
2024-12-02 10:32:27 +0100

    tex shell: Added pandoc for weird conversions and lsps for tex and lang checking

--- shells/tex.nix
+++ shells/tex.nix
@@ -3,10 +3,13 @@
 pkgs.mkShellNoCC {
   buildInputs = with pkgs; [
     texliveFull
+    git
     gnumake
+    ltex-ls
+    pandoc
     poppler_utils
     pdftk
     python3
-    git
+    texlab
   ];
 }
