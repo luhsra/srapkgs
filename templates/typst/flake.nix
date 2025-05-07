40f477f9b2142236f5d29474139fa851ae237068
Illia Ostapyshyn <illia@yshyn.com>
2025-05-07 18:19:39 +0200

    templates: typst: Add Rotis to font paths

--- templates/typst/flake.nix
+++ templates/typst/flake.nix
@@ -3,14 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    srapkgs.url = "github:luhsra/srapkgs/master";
   };
 
-  outputs = { self, nixpkgs, ... }: let
+  outputs = { self, nixpkgs, ... }@inputs: let
     supportedSystems = [ "aarch64-linux" "x86_64-linux"
                          "aarch64-darwin" "x86_64-darwin" ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
-    devShells = forAllSystems (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      srapkgs = inputs.srapkgs.packages.${system};
+    in {
       default = pkgs.mkShellNoCC {
         buildInputs = with pkgs; [
           git
@@ -20,9 +24,8 @@
           typstyle
         ];
         env = {
-          # Make loose fonts available here
-          # By default, this includes fonts contained in our texmf template
-          TYPST_FONT_PATHS = "texmf/fonts/opentype";
+          # Supply Rotis. The package can only be installed from the uni network
+          # TYPST_FONT_PATHS = "${srapkgs.font-rotis}/share/fonts/opentype";
         };
       };
     });
