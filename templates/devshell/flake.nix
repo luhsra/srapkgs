a6d4b47e24577713fca51c6a427e4bef16921ca4
Illia Ostapyshyn <illia@yshyn.com>
2025-05-08 19:38:08 +0200

    templates: Improve devshell

--- templates/devshell/flake.nix
+++ templates/devshell/flake.nix
@@ -2,21 +2,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, ... }: let
-    supportedSystems = [ "aarch64-linux" "x86_64-linux"
-                         "aarch64-darwin" "x86_64-darwin" ];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-  in {
-    devShells = forAllSystems (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = pkgs.mkShellNoCC {
-          buildInputs = with pkgs; [
-            # your inputs here
-          ];
-        };
-      }
-    );
-  };
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              git
+            ];
+          };
+        }
+      );
+    };
 }
