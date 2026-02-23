3b4ecb436abbfe5266571e4505fbb50d574d04f2
Illia Ostapyshyn <illia@yshyn.com>
2026-02-23 10:50:13 +0100

    templates/linux: Make LLVM version easily exchangable

--- templates/linux/meta/nix/flake.nix
+++ templates/linux/meta/nix/flake.nix
@@ -7,38 +7,47 @@
       nixpkgs,
     }:
     let
+      lib = nixpkgs.lib;
       supportedSystems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
       # See https://github.com/NixOS/nixpkgs/issues/368850#issuecomment-3186923233
-      fix-clang-wrapper = _final: prev: {
-        llvmPackages = prev.llvmPackages // {
-          libcxxStdenv = prev.llvmPackages.libcxxStdenv // {
-            mkDerivation =
-              attrs:
-              (prev.llvmPackages.libcxxStdenv.mkDerivation attrs).overrideAttrs (this: {
-                env = (this.env or { }) // {
-                  # Fix `--target` spam.
-                  NIX_CC_WRAPPER_SUPPRESS_TARGET_WARNING = 1;
-                  # Fix `-nostdinc` warnings.
-                  NIX_CFLAGS_COMPILE = prev.lib.concatStringsSep " " [
-                    (this.env.NIX_CFLAGS_COMPILE or "")
-                    "-Wno-unused-command-line-argument"
-                  ];
-                };
-              });
-          };
-        };
-      };
+      # Patch all LLVM versions
+      fix-clang-wrappers =
+        _final: prev:
+        let
+          only-llvmPackages = lib.filterAttrs (n: _: lib.hasPrefix "llvmPackages" n) prev;
+          fix-clang-wrapper =
+            v:
+            v
+            // {
+              stdenv = v.stdenv // {
+                mkDerivation =
+                  attrs:
+                  (v.stdenv.mkDerivation attrs).overrideAttrs (this: {
+                    env = (this.env or { }) // {
+                      # Fix `--target` spam.
+                      NIX_CC_WRAPPER_SUPPRESS_TARGET_WARNING = 1;
+                      # Fix `-nostdinc` warnings.
+                      NIX_CFLAGS_COMPILE = lib.concatStringsSep " " [
+                        (this.env.NIX_CFLAGS_COMPILE or "")
+                        "-Wno-unused-command-line-argument"
+                      ];
+                    };
+                  });
+              };
+            };
+        in
+        builtins.mapAttrs (_: fix-clang-wrapper) only-llvmPackages;
       forEachSupportedSystem =
         f:
-        nixpkgs.lib.genAttrs supportedSystems (
+        lib.genAttrs supportedSystems (
           system:
           f {
             pkgs = import nixpkgs {
               inherit system;
-              overlays = [ fix-clang-wrapper ];
+              overlays = [ fix-clang-wrappers ];
             };
           }
         );
@@ -47,19 +56,9 @@
       # Qemu image for vm based testing
       packages = forEachSupportedSystem (
         { pkgs }:
-        {
-          vm = pkgs.callPackage ./vm.nix { };
-        }
-      );
-
-      apps = forEachSupportedSystem (
-        { pkgs }:
         rec {
-          run-vm = {
-            type = "app";
-            program = "${self.packages.${pkgs.stdenv.hostPlatform.system}.vm}/bin/run-nixos-vm";
-          };
-          default = run-vm;
+          vm = pkgs.callPackage ./vm.nix { };
+          default = vm;
         }
       );
 
@@ -67,15 +66,17 @@
       devShells = forEachSupportedSystem (
         { pkgs }:
         let
+          # NOTE: Replace LLVM version here
+          llvm = pkgs.llvmPackages;
           kernel_pkgs = with pkgs; [
             # Linux kernel extra stuff
             ccache
-            clang
-            lld
-            llvm
+            llvm.clang
+            llvm.lld
+            llvm.llvm
+            llvm.clang-tools
             libgcc
             curlFull
-            clang-tools
             ncurses
             pkg-config
             just
@@ -86,9 +87,7 @@
           default =
             pkgs.mkShell.override
               {
-                # Use clang with libcxx, the only stdlib where std::format is
-                # fully implemented
-                stdenv = pkgs.libcxxStdenv;
+                stdenv = llvm.stdenv;
               }
               {
                 hardeningDisable = [ "all" ];
